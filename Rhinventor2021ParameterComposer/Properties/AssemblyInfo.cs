1d1c3f4c0b1b575427f58408397e9e6012f82a76
Dima <minich.jd@gmail.com>
2022-12-20 20:00:25 +0100

    first commit

---
+++ Rhinventor2021ParameterComposer/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rhinventor2021ParameterComposer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rhinventor2021ParameterComposer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23d2c117-b47f-41cc-9e90-2c35567a3426")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+//1.0.0 - Component created
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Rhinventor2021ParameterComposer/Rhinventor2021ParameterComposerComponent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+
+namespace Rhinventor2021ParameterComposer
+{
+    public class Rhinventor2021ParameterComposerComponent : GH_Component
+    {
+        private static System.Reflection.Assembly ass = System.Reflection.Assembly.GetExecutingAssembly();
+        public Rhinventor2021ParameterComposerComponent()
+          : base("RhinventorParameterComposer", "rhiparam",
+              $"RhinventorParameterComposer {ass.GetName().Version.ToString()} (Created by Dimitrij Minich). Compose parameters",
+              "Rhinventor", "Parameters")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("parameterKey", "K", "Set parameter key", GH_ParamAccess.list);
+            pManager.AddTextParameter("parameterValue", "V", "Set parameter value", GH_ParamAccess.list);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("composedParameter", "P", "Composed parameters", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<string> parameterKeys = new List<string>();
+            if (!DA.GetDataList(0, parameterKeys)) return;
+
+            List<string> parameterValues = new List<string>();
+            if (!DA.GetDataList(1, parameterValues)) return;
+
+            if (parameterKeys.Count != parameterValues.Count) throw new ArgumentException("Length of parameter keys and values is different");
+
+            Dictionary<string, string> composedParameters = new Dictionary<string, string>();
+            for (int i = 0; i < parameterKeys.Count; i++)
+            {
+                string key = parameterKeys[i];
+                string value = parameterValues[i];
+                composedParameters.Add(key, value);
+            }
+
+            DA.SetData(0, composedParameters);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Resources.Rhinventor2021ParameterComposer24;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5ee1453a-bc3e-4ea5-bf9d-254d9fdac7a8"); }
+        }
+    }
+}
